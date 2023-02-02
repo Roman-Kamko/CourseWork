1084918d282beb8e99616a6c6ba6d348dac8cb08
RomanKamko <r.kamko@mail.ru>
2023-02-02 19:20:35 +0400

    Курсовая. Выполнил этапы 1 - 7, 8(a, b, c, d). Базовая сложность.

--- src/edu/skypro/coursework/Main.java
+++ src/edu/skypro/coursework/Main.java
@@ -1,7 +1,71 @@
 package edu.skypro.coursework;
 
 public class Main {
-    public static void main(String[] args) {
+    static Employee[] employeeBook = new Employee[10];
 
+    public static void main(String[] args) {
+        employeeBook[0] = new Employee("Иванов Иван Иванович", 1, 70_000);
+        employeeBook[1] = new Employee("Семенов Семен Семенович", 1, 70_000);
+        employeeBook[2] = new Employee("Андреев Андрей Андреевич", 2, 65_000);
+        employeeBook[3] = new Employee("Петров Петр Петрович", 2, 65_000);
+        employeeBook[4] = new Employee("Сергеев Сергей Сергеевич", 3, 60_000);
+        employeeBook[5] = new Employee("Романов Роман Романович", 3, 60_000);
+        employeeBook[6] = new Employee("Елисеев Елисей Елисеевич", 4, 55_000);
+        employeeBook[7] = new Employee("Парфенов Парфен Парфенович", 4, 55_000);
+        employeeBook[8] = new Employee("Антонов Антон Антонович", 5, 50_000);
+        employeeBook[9] = new Employee("Игнатов Игнат Игнатович", 5, 50_000);
+        printAllEmployees();
+        System.out.println(calculateCostsAmountOnSalaries());
+        System.out.println(findEmployeeWithMinSalary());
+        System.out.println(findEmployeeWithMaxSalary());
     }
+
+    public static void printAllEmployees() {
+        for (int i = 0; i < employeeBook.length; i++) {
+            Employee employee = employeeBook[i];
+            System.out.println("ФИО: " + employee.getFullName() + ", id: " + employee.getId() + ", Отдел: " + employee.getDepartment() + ", Зарплата: " + employee.getSalary());
+        }
+    }
+
+    public static double calculateCostsAmountOnSalaries() {
+        int sum = 0;
+        for (int i = 0; i < employeeBook.length; i++) {
+            sum += employeeBook[i].getSalary();
+        }
+        return sum;
+    }
+
+    public static String findEmployeeWithMinSalary() {
+        double minSalary = employeeBook[0].getSalary();
+        for (int i = 0; i < employeeBook.length; i++) {
+            if (employeeBook[i].getSalary() < minSalary) {
+                minSalary = employeeBook[i].getSalary();
+            }
+        }
+        String employeeWithMinSalary = null;
+        for (int i = 0; i < employeeBook.length; i++) {
+            if (minSalary == employeeBook[i].getSalary()) {
+                employeeWithMinSalary = employeeBook[i].getFullName();
+            }
+        }
+        return employeeWithMinSalary;
+    }
+
+    public static String findEmployeeWithMaxSalary() {
+        double maxSalary = employeeBook[0].getSalary();
+        for (int i = 0; i < employeeBook.length; i++) {
+            if (employeeBook[i].getSalary() > maxSalary) {
+                maxSalary = employeeBook[i].getSalary();
+            }
+        }
+        String employeeWithMaxSalary = null;
+        for (int i = 0; i < employeeBook.length; i++) {
+            if (maxSalary == employeeBook[i].getSalary()) {
+                employeeWithMaxSalary = employeeBook[i].getFullName();
+            }
+        }
+        return employeeWithMaxSalary;
+    }
+
+
 }
